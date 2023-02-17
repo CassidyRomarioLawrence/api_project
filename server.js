5d2fe7fa7159ccded4345c286bd0a731f358586d
CassidyRomarioLawrence <cassidyromario@gmail.com>
2023-02-17 13:44:20 +0200

    structure

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+var corsOption = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOption));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Vans Sneaker Store." });
+});
+
+const PORT = process.env.PORT || 2020;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}.`);
+});
